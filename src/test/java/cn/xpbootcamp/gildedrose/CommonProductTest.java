87451eae07647af7a9fd5bbfaa563ef5ba936a2f
yzehao <yzehao@thoughtworks.com>
2020-05-16 00:57:05 +0800

    common product pass one day sellin -1 and quality -1

---
+++ src/test/java/cn/xpbootcamp/gildedrose/CommonProductTest.java
@@ -0,0 +1,18 @@
+package cn.xpbootcamp.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class CommonProductTest {
+
+    @Test
+    void should_SellIn_0_Quality_39_when_CommonProduct_pass_one_day_given_SellIn_1_Quality_40() {
+        CommonProduct commonProduct = new CommonProduct(1, 40);
+
+        commonProduct.passOneDay();
+
+        assertThat(commonProduct.getSellIn()).isEqualTo(0);
+        assertThat(commonProduct.getQuality()).isEqualTo(39);
+    }
+}
