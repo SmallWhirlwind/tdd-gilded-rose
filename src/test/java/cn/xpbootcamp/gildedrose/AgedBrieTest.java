78b3afea7810c8e47cd6de26916a62f5b86cd2a1
yzehao <yzehao@thoughtworks.com>
2020-05-16 01:08:50 +0800

    aged brie quality is 50 pass one day sellin -1 and quality is 50

--- src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
@@ -15,4 +15,14 @@ public class AgedBrieTest {
         assertThat(agedBrie.getSellIn()).isEqualTo(29);
         assertThat(agedBrie.getQuality()).isEqualTo(41);
     }
+
+    @Test
+    void should_SellIn_29_Quality_50_when_AgedBrie_pass_one_day_given_SellIn_30_Quality_50() {
+        AgedBrie agedBrie = new AgedBrie(30, 50);
+
+        agedBrie.passOneDay();
+
+        assertThat(agedBrie.getSellIn()).isEqualTo(29);
+        assertThat(agedBrie.getQuality()).isEqualTo(50);
+    }
 }
