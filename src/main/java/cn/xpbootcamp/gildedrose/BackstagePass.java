b98139c23c30e054a55dc916abab3aeceb116a6b
yzehao <yzehao@thoughtworks.com>
2020-05-16 01:18:27 +0800

    BackstagePass quality not change when quality >= 50

--- src/main/java/cn/xpbootcamp/gildedrose/BackstagePass.java
+++ src/main/java/cn/xpbootcamp/gildedrose/BackstagePass.java
@@ -19,6 +19,8 @@ public class BackstagePass {
 
     public void passOneDay() {
         this.sellIn -= 1;
-        this.quality += 1;
+        if (this.quality < 50) {
+            this.quality += 1;
+        }
     }
 }
--- src/test/java/cn/xpbootcamp/gildedrose/BackstagePassTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/BackstagePassTest.java
@@ -15,4 +15,14 @@ public class BackstagePassTest {
         assertThat(backstagePass.getSellIn()).isEqualTo(10);
         assertThat(backstagePass.getQuality()).isEqualTo(21);
     }
+
+    @Test
+    void should_SellIn_10_Quality_50_when_BackstagePass_pass_one_day_given_SellIn_11_Quality_50() {
+        BackstagePass backstagePass = new BackstagePass(11, 50);
+
+        backstagePass.passOneDay();
+
+        assertThat(backstagePass.getSellIn()).isEqualTo(10);
+        assertThat(backstagePass.getQuality()).isEqualTo(50);
+    }
 }
